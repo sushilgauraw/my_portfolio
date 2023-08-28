ca7fed61342f692548ebe396e410d8cd253c7700
SushilGauraw <sushilgauraw1996@gmail.com>
2023-08-28 12:58:14 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {Routes,Route,BrowserRouter} from "react-router-dom"
+import {Routes,Route} from "react-router-dom"
 import Home from './Routes/Home';
 import About from './Routes/About';
 import Project from './Routes/Project';
@@ -8,14 +8,13 @@ import Contact from './Routes/Contact';
 function App() {
   return (
     <>
-    <BrowserRouter basename='/my_portfolio'>
+
     <Routes>
-      <Route path='/my_portfolio' element = {<Home/>}/>
+      <Route path='/' element = {<Home/>}/>
       <Route path='/Project' element = {<Project/>}/>
       <Route path='/About' element = {<About/>}/>
       <Route path='/Contact' element = {<Contact/>}/>
     </Routes>
-    </BrowserRouter>
     </>
   );
 }
